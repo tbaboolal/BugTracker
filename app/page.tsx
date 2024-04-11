4784cfb6bc262216acc1d3d70b60faa7363c1291
Emilie Gergi <emiliegergi@Emilies-MacBook-Air.local>
2024-04-11 10:57:13 -0400

    Background

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
 import Image from "next/image";
 import SigninButton from "@/components/SignInButton";
 
-
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="flex flex-col items-center space-y-4">
-      <div className="flex items-center space-x-4">
-         <img src="/roach.gif" className="h-12 w-12" />
-         <h1 className="font-bold">Bug Tracker</h1>
-      </div>
+    <div className="flex justify-center items-center h-screen" style={{ backgroundColor: '#ffd6eb' }}>
+      <div className="text-center">
+        <div className="flex items-center space-x-4 mb-8">
+          <img src="/roach.gif" className="h-12 w-12" />
+          <h1 className="font-comic font-bold text-5xl">Bug Tracker</h1>
+        </div>
         <SigninButton/>
       </div>
     </div>
