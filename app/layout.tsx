c6ab570c353e4f0dedf7559ac8feaf89fe48e81e
Mustafa Khafaji <143281566+mustafakhafaji@users.noreply.github.com>
2024-04-11 10:48:56 -0400

    Homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next';
 import './globals.css';
-import Header from '@/components/Header';
 import Providers from '@/components/Providers';
 
 export const metadata: Metadata = {
@@ -19,7 +18,6 @@ export default function RootLayout({
         
         
           <Providers>
-            <Header /> 
             {children}
             
           </Providers>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 import Image from "next/image";
+import SigninButton from "@/components/SignInButton";
+
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="flex justify-center items-center h-screen">
+      <div className="flex flex-col items-center space-y-4">
+      <div className="flex items-center space-x-4">
+         <img src="/roach.gif" className="h-12 w-12" />
+         <h1 className="font-bold">Bug Tracker</h1>
+      </div>
+        <SigninButton/>
+      </div>
     </div>
   );
 }
