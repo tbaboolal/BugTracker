eb413a10978ec5b183c718f098e4ecdcfc73b828
yusufhamed <Yusuf.hamed04@gmail.com>
2024-04-12 13:47:34 -0400

    Hide header if not signed in

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,32 @@
+'use client';
+
 import Link from 'next/link';
 import Image from 'next/image';
 import SigninButton from './SignInButton';
+import { useSession, SessionProvider  } from 'next-auth/react';
 
 
 function Header() {
+  const { data: session } = useSession();
+
   return (
     <header className='fixed w-full z-50 top-0 flex items-center justify-between p-5'>
-        
-        <div className='flex items-center space-x-2'>
+      <div className='flex items-center space-x-2'>
+        {session && (
           <Link href="/CreateTicketPage/new">
-            <p>create ticket </p> 
+            <p>create ticket</p>
           </Link>
-          
-        </div>
+        )}
+      </div>
     </header>
   );
 }
 
-export default Header;
+export default function HeaderWithSession() {
+  return (
+    <SessionProvider>
+      <Header />
+    </SessionProvider>
+  );
+}
+
